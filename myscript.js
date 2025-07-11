2d5f961219db4ebf595a6fab5dd2ee275722f240
letiof <leletica.oliveira@gmail.com>
2025-07-11 06:13:03 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,8 @@
+ const botao = document.getElementById("botao");
+    
+    botao.addEventListener("click", e => {
+        botao.innerHTML = "Bem vindo ao meu primeiro desenvolvimento de pagina na web :";
+        botao.style.background = "pink";
+        botao.style.fontSize = "20px";
+    });
+   
